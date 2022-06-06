0a770e777466fe8aa1c35ebca53613a64a974a0c
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-06-06 15:44:07 -0400

    Remove extra config

--- variables.tf
+++ variables.tf
@@ -9,14 +9,3 @@ variable "region" {}
 variable "compartment_id" {}
 variable "bucket_name" {}
 variable "bucket_namespace" {}
-
-# Instance Module
-variable "boot_volume_backup_policy" {}
-variable "instance_display_name" {}
-variable "instance_state" {}
-variable "public_ip" {}
-variable "shape" {}
-variable "source_ocid" {}
-variable "ssh_public_keys" {}
-variable "subnet_ocids" {}
-variable "compartment_ocid" {}
