cb46d794342523328703d46ab7181b458b7f8b3f
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-06-06 16:34:40 -0400

    New Instance

--- provider.tf
+++ provider.tf
@@ -4,6 +4,14 @@ provider "oci" {
   user_ocid = var.user_ocid
   fingerprint = var.fingerprint
   private_key = var.private_key
-#  private_key_path = var.private_key_path
-#  private_key_password = var.private_key_password
 }
+
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "4.78.0"
+    }
+  }
+}
+
